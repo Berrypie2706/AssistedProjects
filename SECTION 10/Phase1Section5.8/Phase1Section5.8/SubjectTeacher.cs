3b6fd8013e89f6d6d2e99405d82fe46947a8f756
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-30 17:04:04 +0530

    first commit

---
+++ SECTION 10/Phase1Section5.8/Phase1Section5.8/SubjectTeacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8
+{
+    public class SubjectTeacher : Teacher
+    {
+        private string whichSubject;
+
+        public string WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
+
